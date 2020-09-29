7d8ed7c432a20235143ba96e69483b7611d9e1d0
manas1316am <ad1316a@gmail.com>
2020-09-29 12:32:32 +0530

    Updates

---
+++ precache-manifest.78bb1f7fab2c4ea5cde1e68724324f45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac9bb2949fb16f9d4f53395b98abb2d",
+    "url": "/mygitapp/index.html"
+  },
+  {
+    "revision": "8ac9ab1235f5de8e3750",
+    "url": "/mygitapp/static/css/2.42f58b89.chunk.css"
+  },
+  {
+    "revision": "125d7e32f90590747cef",
+    "url": "/mygitapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ac9ab1235f5de8e3750",
+    "url": "/mygitapp/static/js/2.8e8d1a91.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/mygitapp/static/js/2.8e8d1a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125d7e32f90590747cef",
+    "url": "/mygitapp/static/js/main.d08e5353.chunk.js"
+  },
+  {
+    "revision": "e2233e25a0f456e7455f",
+    "url": "/mygitapp/static/js/runtime-main.67d01e93.js"
+  }
+]);
